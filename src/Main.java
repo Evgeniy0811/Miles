8340bb2dda60ab586407413894c1801b633165be
pros-2007@mail.ru <pros-2007@mail.ru>
2022-01-02 17:56:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+ int ticksPrice = 1000;
+ int miles;
+ int bonus = 20;
+ miles = ticksPrice / bonus;
+         System.out.println(miles);
+    }
+}
